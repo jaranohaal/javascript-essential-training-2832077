7d8c3c2bf7c89aca24f9105ab536842ab91926cd
Maksat Faizuldanov <maksat.faizuldanov@nu.edu.kz>
2022-12-13 05:30:19 +0600

    Practice 03_07 trash can object

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,36 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const trashCan = {
+    volume: 20,
+    color: "black",
+    isFull: false,
+
+    freeTrashCan: function(){
+        if (this.isFull == true){
+            this.isFull = false;
+            console.log("Trash can is empty now");
+        }
+        else{
+            console.log("Trash can is already empty");
+        }
+    },
+
+    putTrash: function(){
+        if(this.isFull == false){
+            this.isFull = true;
+            console.log("You put trash into can")
+        }
+        else{
+            console.log("Trash is already full")
+        }
+    },
+
+    trash:{
+        volume: 20,
+        isOrganic: true,
+        isStinky: true
+    }
+};
