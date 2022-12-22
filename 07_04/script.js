4685043de815cabaa33f5985ac97ea1054a60907
Maksat Faizuldanov <maksat.faizuldanov@nu.edu.kz>
2022-12-22 20:26:15 +0600

    07_04 Practice on arrays

--- 07_04/script.js
+++ 07_04/script.js
@@ -8,4 +8,31 @@
  * - Remove the item you found using the find method from the array.
  */
 
+var isBig = false;
+var numberOfPeople = 2;
 
+const table = ["juice", isBig, "knife", 1, numberOfPeople, 15.7, "mouse", "laptop"]
+
+console.log(table);
+
+var last = table.pop() //Remove last item
+
+console.log("After removing last item ", table);
+
+table.unshift(last) // Add last item as the first
+
+console.log("After adding last item as first", table);
+
+table.sort() //Sort items
+
+console.log("After sorting alphabetically ", table);
+
+console.log(table.find(item=>item =="knife"));
+
+
+console.log(table.find(item=>item =="headset"));
+
+var f = table.indexOf(table.find(item=>item =="knife"))
+delete table[f];
+
+console.log("After deleting knife ",table)
