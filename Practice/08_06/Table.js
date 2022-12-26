5206da02198fed010c947d77d87e57ae8886c958
Maksat Faizuldanov <maksat.faizuldanov@nu.edu.kz>
2022-12-26 21:19:28 +0600

    Practicing Functions 08_06

---
+++ Practice/08_06/Table.js
@@ -0,0 +1,18 @@
+// Class Table
+
+class Table{
+    constructor(
+        size,
+        isComfortable,
+        forEating,
+        location
+    ){
+    this.size = size;
+    this.isComfortable = isComfortable;
+    this.forEating = forEating;
+    this.location = location;
+
+}
+}
+
+export default Table;
--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,31 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+import Table from "./Table.js";
+
+const rentedTable = new Table( // Object rentedTable of class Table
+    100,
+    false,
+    true,
+    "Rented Home"
+);
+
+console.log(rentedTable);
+
+function createArticle(table){ // Function creates article element
+    const main = document.querySelector("main");
+    
+    const content = `
+    <li> The size is: ${table.size}</li>
+    <li> Is it comfortable? ${table.isComfortable}</li>
+    <li> Is it for eating? ${table.forEating}</li>
+    <li> Location: ${table.location}</li>
+    `;
+    const article = document.createElement("article");
+    article.innerHTML = content;
+
+    main.append(article)
+}
+
+createArticle(rentedTable);
