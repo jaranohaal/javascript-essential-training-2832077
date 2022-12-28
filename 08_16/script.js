e35c11c08e4c58cf32e6ae1f4530df1dade1d03e
Maksat Faizuldanov <maksat.faizuldanov@nu.edu.kz>
2022-12-28 21:44:40 +0600

    Challenge creating advanced function 08_16

--- 08_16/script.js
+++ 08_16/script.js
@@ -6,54 +6,50 @@
  * - Append each backpack object to the <main> element.
  */
 import Backpack from "./components/Backpack.js";
+import backpackObjectArray from "./components/data.js";
 
-const everydayPack = new Backpack(
-  "pack01",
-  "Everyday Backpack",
-  30,
-  "grey",
-  15,
-  26,
-  26,
-  false,
-  "December 5, 2018 15:00:00 PST",
-  "../assets/images/everyday.svg"
-);
 
-const content = `
+function advancedFunction(array){
+
+  const main = document.querySelector(".maincontent");
+
+  for(let element of array){
+    const content = `
     <figure class="backpack__image">
-      <img src=${everydayPack.image} alt="" />
+      <img src=${element.image} alt="" />
     </figure>
-    <h1 class="backpack__name">${everydayPack.name}</h1>
+    <h1 class="backpack__name">${element.name}</h1>
     <ul class="backpack__features">
       <li class="packprop backpack__volume">Volume:<span> ${
-        everydayPack.volume
+        element.volume
       }l</span></li>
       <li class="packprop backpack__color">Color:<span> ${
-        everydayPack.color
+        element.color
       }</span></li>
-      <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
+      <li class="backpack__age">Age:<span> ${element.backpackAge()} days old</span></li>
       <li class="packprop backpack__pockets">Number of pockets:<span> ${
-        everydayPack.pocketNum
+        element.pocketNum
       }</span></li>
       <li class="packprop backpack__strap">Left strap length:<span> ${
-        everydayPack.strapLength.left
+        element.strapLength.left
       } inches</span></li>
       <li class="packprop backpack__strap">Right strap length:<span> ${
-        everydayPack.strapLength.right
+        element.strapLength.right
       } inches</span></li>
       <li class="feature backpack__lid">Lid status:<span> ${
-        everydayPack.lidOpen ? "open" : "closed"
+        element.lidOpen ? "open" : "closed"
       }</span></li>
     </ul>
-  
 `;
 
-const main = document.querySelector(".maincontent");
+  const newArticle = document.createElement("article");
+  newArticle.classList.add("backpack");
+  newArticle.setAttribute("id", "everyday");
+  newArticle.innerHTML = content;
 
-const newArticle = document.createElement("article");
-newArticle.classList.add("backpack");
-newArticle.setAttribute("id", "everyday");
-newArticle.innerHTML = content;
+  main.append(newArticle);
+  }
 
-main.append(newArticle);
+}
+
+advancedFunction(backpackObjectArray);
